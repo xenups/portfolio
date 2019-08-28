404a986b32b68aa6c6bb99dec791c944890423b2
xenups <xenups@outlook.com>
2019-08-28 19:08:34 +0430

    Updates

---
+++ precache-manifest.082715c660226ddf65b0b4a5cad10e4e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d64cf0a3ab0a4e3d6c4c6aa85a61fbc9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ca82c1c28568af5f8a5a",
+    "url": "/portfolio/static/css/2.c97396ac.chunk.css"
+  },
+  {
+    "revision": "22674cd4b74a199a6e66",
+    "url": "/portfolio/static/css/main.7c126a7f.chunk.css"
+  },
+  {
+    "revision": "ca82c1c28568af5f8a5a",
+    "url": "/portfolio/static/js/2.793af09f.chunk.js"
+  },
+  {
+    "revision": "22674cd4b74a199a6e66",
+    "url": "/portfolio/static/js/main.0d3069a4.chunk.js"
+  },
+  {
+    "revision": "4bc79a2d54f74fff90c2",
+    "url": "/portfolio/static/js/runtime~main.f7d4dbe8.js"
+  },
+  {
+    "revision": "e61537505e0fd0ad4dc42cf016c20bd0",
+    "url": "/portfolio/static/media/amir.e6153750.jpg"
+  }
+]);

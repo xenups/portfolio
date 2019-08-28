b817fadaea2361975f8693a10c601683a34b05bb
xenups <xenups@outlook.com>
2019-08-28 23:02:59 +0430

    Updates

--- precache-manifest.5057e15dc20d354a1cc62010d04eaf69.js
+++ precache-manifest.5057e15dc20d354a1cc62010d04eaf69.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d64cf0a3ab0a4e3d6c4c6aa85a61fbc9",
+    "revision": "9b14143eb18df35a63746aecfcd12e07",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "ca82c1c28568af5f8a5a",
+    "revision": "d85d6e845de5012bb85d",
     "url": "/portfolio/static/css/2.c97396ac.chunk.css"
   },
   {
-    "revision": "22674cd4b74a199a6e66",
+    "revision": "7c2c053226dbe6906eb0",
     "url": "/portfolio/static/css/main.7c126a7f.chunk.css"
   },
   {
-    "revision": "ca82c1c28568af5f8a5a",
-    "url": "/portfolio/static/js/2.793af09f.chunk.js"
+    "revision": "d85d6e845de5012bb85d",
+    "url": "/portfolio/static/js/2.d9eba334.chunk.js"
   },
   {
-    "revision": "22674cd4b74a199a6e66",
-    "url": "/portfolio/static/js/main.0d3069a4.chunk.js"
+    "revision": "7c2c053226dbe6906eb0",
+    "url": "/portfolio/static/js/main.6f075ebc.chunk.js"
   },
   {
     "revision": "4bc79a2d54f74fff90c2",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.082715c660226ddf65b0b4a5cad10e4e.js"
+  "/portfolio/precache-manifest.5057e15dc20d354a1cc62010d04eaf69.js"
 );
 
 self.addEventListener('message', (event) => {
